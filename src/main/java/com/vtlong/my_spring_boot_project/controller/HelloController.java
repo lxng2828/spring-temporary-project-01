985293daa10a541670e49dd730e91643c3268a46
vtlxng <vietlong.eee@gmail.com>
2025-08-26 17:17:34 +0700

    dads

--- src/main/java/com/vtlong/my_spring_boot_project/controller/HelloController.java
+++ src/main/java/com/vtlong/my_spring_boot_project/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.vtlong.my_spring_boot_project.controller;
 
+import com.vtlong.my_spring_boot_project.response.ApiResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    public ApiResponse<String> hello() {
+        return ApiResponse.success("Hello World");
     }
 }
